f031f5d9e9cb88c0d731cd5763b522c82ad968bd
goofiw <goofiw@gmail.com>
2015-05-23 00:12:54 +0000

    added User#add_office

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,10 @@ class User < ActiveRecord::Base
   validates :email, :username, uniqueness: true, presence: true
   validates :password, presence: true
 
+
+  def add_office(offices_id)
+  	OfficesUsers.new( user_id: @id,
+                      office_id: offices_id
+                      )
+  end
 end
