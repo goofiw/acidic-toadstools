39587e9eb06298f732fcdf0b2133ad9b74e34657
goofiw <goofiw@gmail.com>
2015-05-24 02:42:31 +0000

    changed game list to show active games and associated offices

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,6 +1,10 @@
 class Game < ActiveRecord::Base
 	belongs_to :user
 
+  def getOffice
+  	Office.find(office_id)
+  end
+
   def to_s
   	"ID: #{id} Home: #{user_id} Away: #{visitor_id} office: #{office_id}"
   end
