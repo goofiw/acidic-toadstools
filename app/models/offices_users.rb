00a718cb43690aa420a4e80c142ac740abbc8eb7
goofiw <goofiw@gmail.com>
2015-05-24 01:57:18 +0000

    adding cannot be added to office twice

--- app/models/offices_users.rb
+++ app/models/offices_users.rb
@@ -1,4 +1,13 @@
 class OfficesUsers < ActiveRecord::Base
   belongs_to :user
   belongs_to :office
+
+  validate :only_add_association_once
+
+  def only_add_association_once
+  	puts "Validating"
+  	puts OfficeUsers.find_by(user_id: @user_id, office_id: @office_id)
+  		puts "should throw error!"
+  		errors.add(:duplicate, "We already have you connected with that office")
+  end
 end
