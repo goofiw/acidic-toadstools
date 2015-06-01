34f88f69698bc924b2a52d3e80e47f0344eec2ba
goofiw <goofiw@gmail.com>
2015-06-01 18:49:46 +0000

    fixed heroku error, functional

--- app/twilio_helper.rb
+++ app/twilio_helper.rb
@@ -2,8 +2,6 @@ def twilio_client
 
 	account_sid = ENV['ACCOUNT_SID'] 
 	auth_token = ENV['AUTH_TOKEN'] 
-	puts account_sid 
-	puts auth_token
 	# set up a client to talk to the Twilio REST API 
   Twilio::REST::Client.new account_sid, auth_token 
 end 
